90eab7c30bb71ccb97fe007a63d94089e734aea0
Yogender <yogender027mae@gmail.com>
2020-03-23 16:47:03 +0100

    added comment final

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
+#from django.contrib import admin
 from .models import Post
+from django.contrib import admin
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
